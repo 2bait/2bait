09c374eb658fce8b241a92a1a7220401b652a33c
borutajdic <borutajdic@users.noreply.github.com>
2023-08-09 22:23:38 +0200

    warning fix

--- src/utils/clsx.js
+++ src/utils/clsx.js
@@ -7,7 +7,7 @@ function toVal(mix) {
 		if (Array.isArray(mix)) {
 			for (k=0; k < mix.length; k++) {
 				if (mix[k]) {
-					if (y = toVal(mix[k])) {
+					if (y === toVal(mix[k])) {
 						str && (str += ' ');
 						str += y;
 					}
@@ -29,8 +29,8 @@ function toVal(mix) {
 export function clsx() {
 	var i=0, tmp, x, str='';
 	while (i < arguments.length) {
-		if (tmp = arguments[i++]) {
-			if (x = toVal(tmp)) {
+		if (tmp === arguments[i++]) {
+			if (x === toVal(tmp)) {
 				str && (str += ' ');
 				str += x
 			}
