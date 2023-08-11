8f0ea3cddb379da459787f0f048fb67843b6eee8
borutajdic <borutajdic@users.noreply.github.com>
2023-08-11 23:28:08 +0200

    navigation refactoring

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,27 +1,29 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Link from './link';
 
 type NavigationProps = {
     navigation: object;
-  };
+};
 
-const Navigation: FC<NavigationProps> = ({navigation}) => {
-    
-    console.log(Object.keys(navigation));
-        return (
-            <nav id='nav-wrap' className='opaque'>
-                <a className='mobile-btn' href='#nav-wrap' title='Show navigation'>Show navigation</a>
-                <a className='mobile-btn' href='#' title='Hide navigation'>Hide navigation</a>
-                <ul id='nav' className='nav'>
-                     {Object.keys(navigation).map((key, index) => {
-                        const navigationName = navigation[key as keyof typeof navigation];                        
-                        return (
-                            <Link key={index} link={key} name={navigationName}/>
-                        );
-                    })} 
-                </ul>
-            </nav>
-        );
+const Navigation: FC<NavigationProps> = ({ navigation }) => {
+    const [ navigationOpen, setNavigationOpen ] = useState<boolean>(false);
+
+    const toggleNavigation = () => {
+        setNavigationOpen(!navigationOpen);
+    };
+    return (
+        <nav id='nav-wrap' className='opaque'>
+            <a className='mobile-btn' onClick={toggleNavigation}>Show navigation</a>
+            <ul id='nav' className={navigationOpen ? 'nav close': 'nav'}>
+                {Object.keys(navigation).map((key, index) => {
+                    const navigationName = navigation[key as keyof typeof navigation];
+                    return (
+                        <Link key={index} link={key} name={navigationName} isCurent={false} toggleNavigation={toggleNavigation} />
+                    );
+                })}
+            </ul>
+        </nav>
+    );
 };
 
 export default Navigation;
--- src/components/navigation/link.tsx
+++ src/components/navigation/link.tsx
@@ -3,12 +3,14 @@ import React, { FC } from "react";
 type LinkProps = {
     link: string;
     name: string;
+    isCurent: boolean;
+    toggleNavigation: any;
 };
 
-const Link: FC<LinkProps> = ({ link, name }) => {
+const Link: FC<LinkProps> = ({ link, name,isCurent, toggleNavigation }) => {
     return (
-        <li>
-            <a className='smoothscroll' href={`#${link}`}>{name}</a>
+        <li className={isCurent ? 'current':''}>
+            <a href={`#${link}`} onClick={toggleNavigation}>{name}</a>
         </li>
     )
 };
