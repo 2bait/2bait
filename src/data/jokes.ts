4d34ac097661379846d2d28985f12bb41e8fc50b
borutajdic <borutajdic@users.noreply.github.com>
2023-12-31 20:19:29 +0100

    Update jokes.ts

--- src/data/jokes.ts
+++ src/data/jokes.ts
@@ -5,47 +5,122 @@ const jokes : Joke[] =
         {
             question: "What did the whiskey say to the ice cube?",
             anwser: "You complete me.",
-            category: "M1,W1,W2,X2"
+            category: "M1,W1,X1,W2,X2"
         },
         {
             question: "My wife told me whiskey doesn't solve anything.",
             anwser: "I told her, \"You're right, but it's worth a shot.\"",
-            category: "M1,W1,W2,X2"
+            category: "M1,W1,X1,W2,X2"
         },
         {
             question: "How do computers stay cool during a whiskey night?",
             anwser: "They open Windows.",
-            category: "M1,W1,W2,X2"
+            category: "M1,W1,X1,X2"
         },
         {
             question: "What did the wife say to her husband who was drinking whiskey?",
             anwser: "\"You're on the rocks again, I see.\"",
-            category: "M1,W1,W2,X2"
+            category: "M1,W1,X1,W2,X2"
         },
         {
             question: "A wife asked her husband, \"Why do you love whiskey more than me?\"",
-            anwser: "He replied, \"It's not that I love you any less, but whiskey never complains about my jokes.\"",
-            category: "M1,W1,W2,X2"
+            anwser: "He replied, \"It's not that I love you any less, but whiskey never complains.\"",
+            category: "M1,W1,X1,W2,X2"
         },
         {
             question: "My wife told me to go to the gym.",
             anwser: "So, I went to the gym... Beam, Glenfiddich, and Jameson.",
-            category: "M1,W1,W2,X2"
+            category: "M1,W1,X1,W2,X2"
         },
         {
             question: "A wife asked her husband why he drinks whiskey straight.",
             anwser: "He said, \"Because mixing it would be an unnecessary compromise.\"",
-            category: "M1,W1,W2,X2"
+            category: "M1,W1,X1,W2,X2"
         },
         {
-            question: "Why do husbands and whiskey have something in common?",
+            question: "What do husbands and whiskey have in common?",
             anwser: "They both need time to mature, but at least whiskey has an expiration date.",
-            category: "M1,W1,W2,X2"
+            category: "M1,W1,X1,W2,X2"
         },
         {
             question: "My husband says he's like fine whiskey – getting better with age.",
             anwser: "I told him he's more like a good bourbon: a little sweet and a bit corny.",
-            category: "M1,W1,W2,X2"
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "What's a wife's advice for handling a husband's whiskey hobby?",
+            anwser: "\"Just let him enjoy it; it's cheaper than therapy.\"",
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "Why did the wife compare her husband's jokes to whiskey?",
+            anwser: "\"They both get better after the first few sips.\"",
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "What did the wife say when her husband claimed he was a whiskey expert?",
+            anwser: "\"If only you were as good at fixing the sink.\"",
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "What's a wife's response when her friends ask about her husband's whiskey hobby?",
+            anwser: "\"It's his way of collecting memories – in a bottle.\"",
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "Why did the wife ask for gin when she saw her husband fixing things?",
+            anwser: "She needed a sip of patience while watching the handyman in action.",
+            category: "M1,W1,X1,G2,X2"
+        },
+        {
+            question: "How does whiskey and gin resolve a disagreement?",
+            anwser: "They mix their differences and create a cocktail of compromise.",
+            category: "M1,W1,X1,W2,G2,X2"
+        },
+        {
+            question: "Why did the gin break up with soda?",
+            anwser: "It wanted a relationship with a little more bite.",
+            category: "M1,W1,X1,W2,G2,X2"
+        },
+        {
+            question: "How do whiskey and gin handle stress during deadlines?",
+            anwser: "One on the rocks, the other with a twist.",
+            category: "M1,W1,X1,W2,G2,X2"
+        },
+        {
+            question: "How many coworkers does it take to open a bottle of whiskey?",
+            anwser: "None, that's what interns are for!",
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "What did the gin say to the tonic?",
+            anwser: "\"You're the mixer that makes me sparkle!\"",
+            category: "M1,W1,X1,G2,X2"
+        },
+        {
+            question: "Why did the husband's whiskey bottle come with a warranty?",
+            anwser: "Because it was afraid of being on the rocks too often!",
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "My husband said he's the master of multitasking at work.",
+            anwser: "I said, \"You mean juggling emails and daydreaming about whiskey?\"",
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "What's the difference between a husband and a glass of whiskey?",
+            anwser: "Whiskey eventually runs out, but a husband's puns seem to have no bottom.",
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "Why did the wife bring whiskey to the office?",
+            anwser: "To show her coworkers what a smooth blend of work and home life looks like.",
+            category: "M1,W1,X1,W2,X2"
+        },
+        {
+            question: "How does a wife keep her husband happy?",
+            anwser: "A splash of affection and a twist of understanding – just like a good whiskey.",
+            category: "M1,W1,X1,W2,X2"
         }
     ];
 
