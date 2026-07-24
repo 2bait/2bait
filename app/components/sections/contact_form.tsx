53c516602bc078d7ded9d0de73f89fce23fd72d3
borutajdic <borutajdic@users.noreply.github.com>
2026-07-24 18:06:39 +0200

    Fix netlify contact form

--- app/components/sections/contact_form.tsx
+++ app/components/sections/contact_form.tsx
@@ -11,11 +11,15 @@ export default function ContactForm ({}:ContactFormProps) {
             method="POST"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
-            action="/.netlify/functions/contact"  
             autoComplete='on'
             className="card contact-card fade-in-up"
         >
             <input type="hidden" name="form-name" value="contact" />
+            <p style={{ display: 'none' }}>
+                <label>
+                    Don&apos;t fill this out: <input name="bot-field" />
+                </label>
+            </p>
             <div className="form-row">
                 <div className="form-field">
                     <label htmlFor="name">Name</label>
